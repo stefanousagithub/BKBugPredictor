c98d70d880f128931b2d0e80eb6601a400175e00
StefanoC <stefano.costanzo.97@gmail.com>
2023-05-14 17:24:48 +0200

    commit a bug: division by 0

--- src/main/java/RetrieveTicketsID.java
+++ src/main/java/RetrieveTicketsID.java
@@ -45,27 +45,31 @@ public class RetrieveTicketsID {
 
   
   	   public static void main(String[] args) throws IOException, JSONException {
-		   
-		   String projName ="BOOKKEEPER";
-	   Integer j = 0, i = 0, total = 1;
-      //Get JSON API for closed bugs w/ AV in the project
-      do {
-         //Only gets a max of 1000 at a time, so must do this multiple times if bugs >1000
-         j = i + 1000;
-         String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
-                + projName + "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR"
-                + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
-                + i.toString() + "&maxResults=" + j.toString();
-         JSONObject json = readJsonFromUrl(url);
-         JSONArray issues = json.getJSONArray("issues");
-         total = json.getInt("total");
-         for (; i < total && i < j; i++) {
-            //Iterate through each bug
-            String key = issues.getJSONObject(i%1000).get("key").toString();
-            System.out.println(key);
-         }  
-      } while (i < total);
-      return;
+  		 	   
+  		  // Add a bug for sonarcloud 
+  		  int x = 10 / 0;
+  		  System.out.println(x);
+  		  
+		  String projName ="BOOKKEEPER";
+		  Integer j = 0, i = 0, total = 1;
+	      //Get JSON API for closed bugs w/ AV in the project
+	      do {
+	         //Only gets a max of 1000 at a time, so must do this multiple times if bugs >1000
+	         j = i + 1000;
+	         String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
+	                + projName + "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR"
+	                + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
+	                + i.toString() + "&maxResults=" + j.toString();
+	         JSONObject json = readJsonFromUrl(url);
+	         JSONArray issues = json.getJSONArray("issues");
+	         total = json.getInt("total");
+	         for (; i < total && i < j; i++) {
+	            //Iterate through each bug
+	            String key = issues.getJSONObject(i%1000).get("key").toString();
+	            System.out.println(key);
+	         }  
+	      } while (i < total);
+	      return;
    }
 
  
